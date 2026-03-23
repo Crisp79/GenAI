731fb37719865a8ce3f0640cc47618de73352c17
crisp79 <akhirangoli@gmail.com>
2026-03-24 00:34:22 +0530

    added images

--- src/models/vae.py
+++ src/models/vae.py
@@ -6,55 +6,42 @@ import torch.nn as nn
 class ResBlock(nn.Module):
     def __init__(self, channels):
         super().__init__()
-        self.block = nn.Sequential(
-            nn.Conv2d(channels, channels, 3, 1, 1),
-            nn.ReLU(),
-            nn.Conv2d(channels, channels, 3, 1, 1),
-        )
+        self.conv1 = nn.Conv2d(channels, channels, 3, 1, 1)
+        self.conv2 = nn.Conv2d(channels, channels, 3, 1, 1)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
-        return x + self.block(x)
+        out = self.relu(self.conv1(x))
+        out = self.conv2(out)
+        return x + out
 
 
 # 🔷 Encoder
 class Encoder(nn.Module):
-    def __init__(
-        self,
-        in_channels=3,
-        hidden_dims=[32, 64, 128, 256],
-        latent_dim=128,
-        kernel_size=4,
-        stride=2,
-        padding=1,
-        use_residual=False,
-        input_size=64,
-    ):
+    def __init__(self, in_channels, hidden_dims, latent_dim,
+                 kernel_size, stride, padding, use_residual, input_size):
         super().__init__()
 
-        modules = []
-        current_channels = in_channels
+        layers = []
+        c = in_channels
 
-        for h_dim in hidden_dims:
-            modules.append(
-                nn.Sequential(
-                    nn.Conv2d(current_channels, h_dim, kernel_size, stride, padding),
-                    nn.ReLU(),
-                )
-            )
+        for h in hidden_dims:
+            layers.append(nn.Conv2d(c, h, kernel_size, stride, padding))
+            layers.append(nn.ReLU())
 
             if use_residual:
-                modules.append(ResBlock(h_dim))
+                layers.append(ResBlock(h))
 
-            current_channels = h_dim
+            c = h
 
-        self.conv = nn.Sequential(*modules)
+        self.conv = nn.Sequential(*layers)
         self.flatten = nn.Flatten()
 
-        # 🔥 Dynamically compute feature map size
+        # 🔥 auto compute shape
         with torch.no_grad():
             dummy = torch.zeros(1, in_channels, input_size, input_size)
             out = self.conv(dummy)
-            self.feature_shape = out.shape[1:]  # (C, H, W)
+            self.feature_shape = out.shape[1:]
             self.flatten_dim = out.view(1, -1).shape[1]
 
         self.fc_mu = nn.Linear(self.flatten_dim, latent_dim)
@@ -63,78 +50,52 @@ class Encoder(nn.Module):
     def forward(self, x):
         x = self.conv(x)
         x = self.flatten(x)
-        mu = self.fc_mu(x)
-        logvar = self.fc_logvar(x)
-        return mu, logvar
+        return self.fc_mu(x), self.fc_logvar(x)
 
 
 # 🔷 Decoder
 class Decoder(nn.Module):
-    def __init__(
-        self,
-        feature_shape,
-        out_channels=3,
-        hidden_dims=[32, 64, 128, 256],
-        latent_dim=128,
-        kernel_size=4,
-        stride=2,
-        padding=1,
-        use_residual=False,
-    ):
+    def __init__(self, feature_shape, hidden_dims, latent_dim,
+                 kernel_size, stride, padding, use_residual):
         super().__init__()
 
-        self.feature_shape = feature_shape
         hidden_dims = hidden_dims[::-1]
+        self.feature_shape = feature_shape
 
         self.fc = nn.Linear(latent_dim, int(torch.prod(torch.tensor(feature_shape))))
 
-        modules = []
-        current_channels = hidden_dims[0]
-
+        layers = []
         for i in range(len(hidden_dims) - 1):
-            modules.append(
-                nn.Sequential(
-                    nn.ConvTranspose2d(
-                        hidden_dims[i],
-                        hidden_dims[i + 1],
-                        kernel_size,
-                        stride,
-                        padding,
-                    ),
-                    nn.ReLU(),
-                )
+            layers.append(
+                nn.ConvTranspose2d(hidden_dims[i], hidden_dims[i+1],
+                                   kernel_size, stride, padding)
             )
+            layers.append(nn.ReLU())
 
             if use_residual:
-                modules.append(ResBlock(hidden_dims[i + 1]))
+                layers.append(ResBlock(hidden_dims[i+1]))
 
-        self.deconv = nn.Sequential(*modules)
+        self.deconv = nn.Sequential(*layers)
 
-        self.final_layer = nn.Sequential(
-            nn.ConvTranspose2d(
-                hidden_dims[-1],
-                out_channels,
-                kernel_size,
-                stride,
-                padding,
-            ),
-            nn.Sigmoid(),
+        self.final = nn.Sequential(
+            nn.ConvTranspose2d(hidden_dims[-1], 3,
+                               kernel_size, stride, padding),
+            nn.Sigmoid()
         )
 
     def forward(self, z):
         x = self.fc(z)
         x = x.view(-1, *self.feature_shape)
         x = self.deconv(x)
-        x = self.final_layer(x)
-        return x
+        return self.final(x)
 
 
-# 🔷 VAE Wrapper
+# 🔷 VAE
 class VAE(nn.Module):
     def __init__(
         self,
-        latent_dim=128,
-        hidden_dims=[32, 64, 128, 256],
+        latent_dim=64,
+        hidden_dims=[32, 64, 128],
         kernel_size=4,
         stride=2,
         padding=1,
@@ -143,35 +104,24 @@ class VAE(nn.Module):
     ):
         super().__init__()
 
-        # Encoder
         self.encoder = Encoder(
-            latent_dim=latent_dim,
-            hidden_dims=hidden_dims,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            use_residual=use_residual,
-            input_size=input_size,
+            3, hidden_dims, latent_dim,
+            kernel_size, stride, padding,
+            use_residual, input_size
         )
 
-        # Decoder (uses encoder's computed shape)
         self.decoder = Decoder(
-            feature_shape=self.encoder.feature_shape,
-            latent_dim=latent_dim,
-            hidden_dims=hidden_dims,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            use_residual=use_residual,
+            self.encoder.feature_shape,
+            hidden_dims, latent_dim,
+            kernel_size, stride, padding,
+            use_residual
         )
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return mu + eps * std
+        return mu + torch.randn_like(std) * std
 
     def forward(self, x):
         mu, logvar = self.encoder(x)
         z = self.reparameterize(mu, logvar)
-        recon = self.decoder(z)
-        return recon, mu, logvar
+        return self.decoder(z), mu, logvar
